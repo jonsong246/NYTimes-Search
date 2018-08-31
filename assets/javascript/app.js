acab97848bdfe3ad208d6486134ddd55a5933ef7
BBUTH <BBUTH_EMAIL>
2018-08-30 19:51:13 -0700

    js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,61 @@
 
-let q = []
+let url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+//&begin_date=${begin_date}&end_date=${end_date}
+let apikey = '896fe5f1ca604377b0fc87c58211f0ec'
+let q = 'test'
+let fq = ''
+let begin_date = '20170210'
+let end_date = ''
+let sort = ''
+let fl = ''
+let hl = ''
+let page = '1'
+let facet_field = ''
+let facet_filter = ''
+
+function buildURL(){
+    let tempURL = url + `?api-key=${apikey}`
+    if(q.length > 0){
+        tempURL = tempURL + `&q=${q}`
+    }
+    if(page.length > 0){
+        tempURL = tempURL + `&page=${page}`
+    }
+    if(begin_date > 0){
+        tempURL = tempURL + `&begin_date=${begin_date}`
+    }
+    if(end_date > 0){
+        tempURL = tempURL + `&end_date=${end_date}`
+    }
+    console.log(`build url: ${tempURL}`)
+    return tempURL
+}
 
 function searchAPI(){
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-    url += '?' + $.param({
-    'api-key': "896fe5f1ca604377b0fc87c58211f0ec",
-    'q': `${q}`
-    :
-    });
+    var url = ``
+    url = buildURL()
+
     $.ajax({
     url: url,
     method: 'GET',
-    }).done(function(result) {
-    console.log(result);
+    }).done(function(results) {
+        console.log(results)
+        let arr = results.response.docs
+
+        arr.forEach(result => {
+            console.log(result.headline.main)
+        })
     }).fail(function(err) {
-    throw err;
+        throw err;
     });
 }
 
-$(document).ready(function() {
+//running here
+    console.log('running search API')
+    searchAPI()
+
     $(document).on("click",'.submitBtn', function(){
         // get form data
         searchAPI()
     })
 
-}
